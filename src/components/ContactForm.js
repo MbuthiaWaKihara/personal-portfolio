f935c0a684859752719973186b1805d57569f9ca
Evanson Mbuthia <kiharambuthia@gmail.com>
2021-08-12 20:03:52 +0300

    remove deploy warning

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -48,8 +48,9 @@ const ContactForm = () => {
   });
 
   const isEmail = (email) => {
-    //eslint-disable-next-line
+    
     let emailRegEx =
+    //eslint-disable-next-line
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (email.trim().match(emailRegEx)) return true;
     return false;
